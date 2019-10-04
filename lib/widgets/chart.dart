7251a3ecabf9d11a92ba80eb265b7f8889e2a298
Calm Andy <thought7878@qq.com>
2019-10-04 16:42:36 +0800

    95. Planning the Chart Widget

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(20),
+      elevation: 6,
+      child: Container(),
+    );
+  }
+}
