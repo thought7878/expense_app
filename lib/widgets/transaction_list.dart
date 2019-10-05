2728346cbca1f62d9c55f96b00fb4023ba7841d6
Calm Andy <thought7878@qq.com>
2019-10-05 13:14:46 +0800

    103. Adding a ListTile Widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -31,49 +31,73 @@ class TransactionList extends StatelessWidget {
               itemCount: transactions.length,
               itemBuilder: (cxt, index) {
                 return Card(
-                  // color: Colors.red,
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(4),
-                        child: Text(
-                          '\$${transactions[index].price.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 6),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(
+                            child: Text(
+                                '\$${transactions[index].price.toStringAsFixed(0)}')),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transactions[index].title,
-                            style: Theme.of(context).textTheme.title,
-                          ),
-                          Text(
-                            DateFormat('yyyy-MM-dd HH:mm:ss')
-                                .format(transactions[index].date),
-                            // DateFormat.yMMMMd('zh_CN').format(tx.date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(DateFormat.yMd('zh_CN')
+                        .format(transactions[index].date)),
                   ),
                 );
               },
             ),
+    );
+  }
+
+  Widget customListItem(BuildContext context, int index) {
+    return Card(
+      // color: Colors.red,
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Theme.of(context).primaryColor,
+                width: 2,
+              ),
+            ),
+            padding: EdgeInsets.all(4),
+            child: Text(
+              '\$${transactions[index].price.toStringAsFixed(2)}',
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                transactions[index].title,
+                style: Theme.of(context).textTheme.title,
+              ),
+              Text(
+                DateFormat('yyyy-MM-dd HH:mm:ss')
+                    .format(transactions[index].date),
+                // DateFormat.yMMMMd('zh_CN').format(tx.date),
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
